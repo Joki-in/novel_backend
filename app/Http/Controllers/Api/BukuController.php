f66b99d93d6016b94b4a1f0f061d5c599ecb6d7a
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2024-02-05 21:50:19 +0700

    .

--- app/Http/Controllers/Api/BukuController.php
+++ app/Http/Controllers/Api/BukuController.php
@@ -11,9 +11,9 @@ class BukuController extends Controller
 {
     public function TopLikedBooks()
     {
-        $topLikedBooks = Buku::select('buku.id', 'buku.sinopsis', 'buku.view', 'buku.genre', 'buku.cover', 'buku.penulis_id', DB::raw('COUNT(like.id) as like_count'))
+        $topLikedBooks = Buku::select('buku.id', 'buku.judul', 'buku.sinopsis', 'buku.view', 'buku.genre', 'buku.cover', 'buku.penulis_id', DB::raw('COUNT(like.id) as like_count'))
             ->leftJoin('like', 'buku.id', '=', 'like.buku_id')
-            ->groupBy('buku.id', 'buku.sinopsis', 'buku.view', 'buku.genre', 'buku.cover', 'buku.penulis_id')
+            ->groupBy('buku.id', 'buku.judul', 'buku.sinopsis', 'buku.view', 'buku.genre', 'buku.cover', 'buku.penulis_id')
             ->orderByDesc('like_count')
             ->take(3)
             ->get();
@@ -23,6 +23,7 @@ class BukuController extends Controller
             'data' => $topLikedBooks,
         ]);
     }
+
     public function topView()
     {
         // Mengambil 3 buku dengan jumlah view terbanyak, diurutkan secara descending
