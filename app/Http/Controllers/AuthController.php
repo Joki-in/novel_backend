7a2cf468f5f7ae73d632a15b8df7159ed37440e9
Rizqi Nur Andi Putra <thecaracter1@gmail.com>
2024-06-26 01:59:52 +0700

    perubahan

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function indexLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function storeLogin(Request $request)
+    {
+        // Validasi data masukan dari formulir login
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Coba untuk melakukan otentikasi pengguna
+        if (Auth::attempt($credentials)) {
+
+            return redirect()->intended('/dashboardAdmin');
+        }
+
+        // Jika otentikasi gagal, kembalikan ke halaman login dengan pesan kesalahan
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        // Invalidate the user's session
+        $request->session()->invalidate();
+
+        // Regenerate the CSRF token
+        $request->session()->regenerateToken();
+
+        // Redirect the user to the login page or wherever you want
+        return redirect('/')->with('message', 'You have been successfully logged out!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+<<<<<<< Updated upstream
+=======
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+>>>>>>> Stashed changes
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+<<<<<<< Updated upstream
 Route::get('/', function () {
     return view('welcome');
 });
+=======
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'indexLogin']);
+Route::post('/masuk', [AuthController::class, 'storeLogin'])->name('login');
+
+Route::get('/register', [AuthController::class, 'registerShow']);
+Route::post('/register', [AuthController::class, 'storeRegister'])->name('register');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+//admin
+// Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::middleware(['islogin'])->group(function () {
+    Route::get('/dashboardAdmin', [DashboardController::class, 'index'])->name('dashboard.index');
+
+});
+>>>>>>> Stashed changes
