a50aed3764393cb22330b3e4d51e93abd6f00c36
Rizqi Nur Andi Putra <e31211961@student.polije.ac.id>
2024-06-30 20:29:54 +0700

    Update TerimaBukuController.php

--- app/Http/Controllers/TerimaBukuController.php
+++ app/Http/Controllers/TerimaBukuController.php
@@ -14,25 +14,27 @@ class TerimaBukuController extends Controller
     {
         $buku = Buku::where('status', 'belum diterima')
             ->with('penulis')
-            ->orderBy('update_at', 'asc');
+            ->orderBy('updated_at', 'asc')
+            ->get();
 
         return view('page.admin.terimabuku', compact('buku'));
     }
 
+
     public function terimaBuku($id)
     {
-        // Temukan buku berdasarkan ID
+
         $buku = Buku::find($id);
 
-        // Pastikan buku ditemukan
+
         if (!$buku) {
             return response()->json(['message' => 'Buku tidak ditemukan.'], 404);
         }
 
-        // Update status menjadi "diterima"
+
         $buku->update(['status' => 'diterima']);
 
-        // Berikan respons sukses
+
         $notification = [
             'title' => 'Selamat!',
             'text' => 'Buku berhasil diterbitkan',
@@ -43,25 +45,25 @@ class TerimaBukuController extends Controller
     }
     public function tolakBuku(Request $request, $id)
     {
-        // Temukan buku berdasarkan ID
+
         $buku = Buku::find($id);
 
-        // Pastikan buku ditemukan
+
         if (!$buku) {
             return response()->json(['message' => 'Buku tidak ditemukan.'], 404);
         }
 
-        // Validasi alasan penolakan
+
         $request->validate([
             'alasan' => 'required|string|max:255',
         ]);
 
-        // Update status menjadi "ditolak" dan simpan alasan
+
         $buku->status = 'ditolak';
         $buku->alasan = $request->alasan;
         $buku->save();
 
-        // Berikan respons sukses
+
         $notification = [
             'title' => 'Buku Ditolak',
             'text' => 'Buku berhasil ditolak dengan alasan: ' . $request->alasan,
